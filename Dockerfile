508e6074247e4be9325d2c7c9762ec4f6fb266c7
novice <david@cninone.com>
2019-03-06 22:43:14 +0800

    add some packages for testing

--- Dockerfile
+++ Dockerfile
@@ -5,25 +5,8 @@ LABEL maintainer="David <david@cninone.com>"
 #    debconf: unable to initialize frontend: Dialog
 ENV DEBIAN_FRONTEND noninteractive
 
-RUN apt-get update -y && apt-get install -y \
-    curl libltdl-dev gnupg locales tzdata 
-     
-RUN locale-gen en_US.UTF-8 zh_CN.UTF-8 ; mkdir -p /var/run/sshd
+COPY ins_pack.sh /ins_pack.sh
+RUN /ins_pack.sh
 
-ENV LANG en_US.UTF-8
-RUN { \
-        echo "LANG=$LANG"; \
-        echo "LANGUAGE=$LANG"; \
-        echo "LC_ALL=$LANG"; \
-} > /etc/default/locale
-RUN useradd -ms /bin/bash david && usermod -aG sudo david ; \
-        echo 'david:freego' | chpasswd ; echo 'root:freego_2019' | chpasswd
-
-
-ENV TZ=Asia/Chongqing
-RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-
-RUN curl -sL https://deb.nodesource.com/setup_11.x | bash - && apt-get install -y nodejs
-# apt-get install -y build-essential
 
 # ENTRYPOINT ["/init.sh"]
---
+++ ins_pack.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+apt-get update -y && apt-get install -y \
+    curl libltdl-dev gnupg locales tzdata 
+     
+locale-gen en_US.UTF-8 zh_CN.UTF-8 ; mkdir -p /var/run/sshd
+
+LANG=en_US.UTF-8
+{ \
+        echo "LANG=$LANG"; \
+        echo "LANGUAGE=$LANG"; \
+        echo "LC_ALL=$LANG"; \
+} > /etc/default/locale
+useradd -ms /bin/bash david && usermod -aG sudo david ; \
+        echo 'david:freego' | chpasswd ; echo 'root:freego_2019' | chpasswd
+
+
+TZ=Asia/Chongqing
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+# php begin
+wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add - \
+	&& echo "deb https://packages.sury.org/php/ stretch main" | tee /etc/apt/sources.list.d/php.list
+# php end
+# nodejs begin
+curl -sL https://deb.nodesource.com/setup_11.x | bash - 
+
+apt-get update && apt-get install -y nodejs build-essential python3-pip python-pip \
+	php7.3 php7.3-bcmath php7.3-bz2 php7.3-cli php7.3-common php7.3-curl php7.3-dba php7.3-dev php7.3-enchant \
+	php7.3-fpm php7.3-gd php7.3-gmp php7.3-imap php7.3-intl php7.3-json php7.3-mbstring php7.3-mysql php7.3-odbc \
+	php7.3-opcache php7.3-pgsql php7.3-readline \
+	php7.3-soap php7.3-sqlite3 php7.3-tidy php7.3-xml php7.3-xmlrpc php7.3-zip php-redis php-igbinary php-mongodb 
+
+curl -o /usr/local/bin/composer https://getcomposer.org/download/1.8.4/composer.phar && chmod +x /usr/local/bin/composer
+npm install -g pkg
+pip install pyinstaller
+
+rm -- "$0"
