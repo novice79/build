2764ee5ddad206a2e72d3af15044f26ae481e4c9
novice <david@cninone.com>
2019-04-08 22:05:51 +0800

    change cd dir

--- ins_pack.sh
+++ ins_pack.sh
@@ -43,14 +43,14 @@ pkg -t node10-linux app.js
 # retain python app for test?
 
 wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.bz2
-tar Jxvf boost*.tar.bz2
-cd boost* && ./bootstrap.sh
+tar jxf boost*.tar.bz2
+cd boost_1_69_0 && ./bootstrap.sh
 ./b2 threading=multi threadapi=pthread link=static runtime-link=static install
 cd ..
 rm -rf boost*
 wget https://github.com/Kitware/CMake/releases/download/v3.14.1/cmake-3.14.1-Linux-x86_64.tar.gz
-tar zxvf cmake*.tar.gz
-cd cmake*
+tar zxf cmake*.tar.gz
+cd cmake-3.14.1-Linux-x86_64
 cp -r bin /usr/
 cp -r share /usr/
 cp -r doc /usr/share/
