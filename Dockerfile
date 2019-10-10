7fff8f1839b952ceee5a3afc7e9848598929d956
novice <novice@piaoyun.shop>
2019-10-10 11:38:34 +0800

    add node and cmake

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,11 @@ LABEL maintainer="Novice <novice@piaoyun.shop>"
 # Get noninteractive frontend for Debian to avoid some problems:
 #    debconf: unable to initialize frontend: Dialog
 ARG DEBIAN_FRONTEND=noninteractive
-ENV LANG en_US.UTF-8  
-ENV LANGUAGE en_US:en  
+# ENV LANG en_US.UTF-8  
+# ENV LANGUAGE en_US:en  
 
 COPY ins_pack.sh /ins_pack.sh
 RUN /ins_pack.sh
 
-ENV LC_ALL en_US.UTF-8   
+# ENV LC_ALL en_US.UTF-8   
 
--- ins_pack.sh
+++ ins_pack.sh
@@ -14,14 +14,19 @@ cd vcpkg
 ./vcpkg install boost:x64-linux dlib:x64-linux opencv:x64-linux
 # CMake projects should use: "-DCMAKE_TOOLCHAIN_FILE=/vcpkg/scripts/buildsystems/vcpkg.cmake"
 
-# cd /tmp
-# wget https://github.com/Kitware/CMake/releases/download/v3.14.1/cmake-3.14.1-Linux-x86_64.tar.gz
-# tar zxf cmake*.tar.gz
-# cd cmake-3.14.1-Linux-x86_64
-# cp -r bin /usr/
-# cp -r share /usr/
-# cp -r doc /usr/share/
-# cp -r man /usr/share/
-# cd ..
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
+apt-get install -y nodejs
+npm install -g cmake-js pkg
+
+cd /tmp
+wget https://github.com/Kitware/CMake/releases/download/v3.15.4/cmake-3.15.4-Linux-x86_64.tar.gz
+tar zxf cmake*.tar.gz
+cd cmake-*-Linux-x86_64
+cp -r bin /usr/
+cp -r share /usr/
+cp -r doc /usr/share/
+cp -r man /usr/share/
+rm -rf *
+cd ..
 
 rm -- "$0"
