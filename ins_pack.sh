b863248813d229cb3d817eb26f86dc6051c57080
novice <novice@piaoyun.shop>
2019-10-14 13:49:06 +0800

    install node-gyp

--- ins_pack.sh
+++ ins_pack.sh
@@ -5,5 +5,11 @@ cd /vcpkg
 
 ./vcpkg install dlib:x64-linux opencv:x64-linux 
 # CMake projects should use: "-DCMAKE_TOOLCHAIN_FILE=/vcpkg/scripts/buildsystems/vcpkg.cmake"
+# base image already: npm install -g cmake-js pkg 
+npm install -g node-gyp
+cd /tmp
+echo "console.log('Hello world');" >  app.js
+pkg -t node12-linux app.js
+rm *
 
 rm -- "$0"
