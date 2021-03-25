47413bcc463f499ed20e00814423b3a3d16f7e5e
novice <novice@piaoyun.shop>
2021-03-25 21:36:02 +0800

    build boost and libtorrent

--- ins_pack.sh
+++ ins_pack.sh
@@ -33,10 +33,32 @@ curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
 apt-get update && apt-get install -y \
 nodejs build-essential \
 gcc-10 g++-10 cmake \
-zlib1g-dev libssl-dev libboost-all-dev \
+zlib1g-dev libssl-dev \
 libsqlite3-dev libmagic-dev
 
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9  9  --slave /usr/bin/g++ g++ /usr/bin/g++-9
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 10 --slave /usr/bin/g++ g++ /usr/bin/g++-10
+
+cd /tmp
+boost_ver=1_75_0
+wget "https://dl.bintray.com/boostorg/release/${boost_ver//_/.}/source/boost_$boost_ver.tar.bz2"
+tar jxf boost_$boost_ver.tar.bz2
+# can not cd *xx*xx*
+cd boost_$boost_ver && ./bootstrap.sh
+./b2 cxxflags=-fPIC link=static runtime-link=shared threading=multi threadapi=pthread install
+cd /tmp
+lt_ver="2.0.2"
+wget https://github.com/arvidn/libtorrent/releases/download/v$lt_ver/libtorrent-rasterbar-$lt_ver.tar.gz
+tar zxf libtorrent-rasterbar-$lt_ver.tar.gz
+echo "using gcc ;" > /tmp/user-config.jam
+export BOOST_ROOT="/tmp/boost_$boost_ver"
+export BOOST_BUILD_PATH=$BOOST_ROOT/tools/build
+export PATH="$BOOST_ROOT:$PATH"
+cd libtorrent-rasterbar-$lt_ver
+b2 -j4 --user-config=/tmp/user-config.jam \
+crypto=openssl cxxstd=20 \
+boost-link=static logging=off dht=on \
+threading=multi threadapi=pthread link=static runtime-link=shared \
+install
 
 # rm -- "$0"
