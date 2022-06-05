0e6502d3e9b6b63a6e5115016dadee33102dd8b8
jiangwei <wwpptogether@gmail.com>
2022-06-05 23:43:40 +0800

    use lsb_release get ubuntu code name

--- ins_pack.sh
+++ ins_pack.sh
@@ -20,7 +20,7 @@ https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null \
 | gpg --dearmor - \
 | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 
-apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
+apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -s -c) main"
 
 
 
