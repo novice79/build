bd5386349b8320882d3f291d2ba2e504b5eed8b3
jiangwei <wwpptogether@gmail.com>
2022-10-04 03:35:44 +0800

    add some memo

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 docker buildx create --name mybuilder --driver docker-container --bootstrap --use
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t novice/build:alpine --push .
+
+# docker buildx imagetools inspect novice/build:alpine
+# docker pull --platform linux/arm/v7 novice/build:alpine
+# docker run --platform linux/arm/v7 -it --rm --name tt novice/build:alpine
