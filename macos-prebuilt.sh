35f211158d859576c3a5359a28e5c3ac1a766158
jiangwei <wwpptogether@gmail.com>
2023-05-02 14:26:27 +0800

    change openssl-3.1.0 to openssl-1.1.1t for macos prebuilt

--- macos-prebuilt.sh
+++ macos-prebuilt.sh
@@ -3,15 +3,20 @@ set -e
 # set -x
 cd /tmp
 ZLIB_ROOT="/tmp/zlib-1.2.13"
-OPENSSL_ROOT="/tmp/openssl-3.1.0"
+# openssl="openssl-3.1.0"
+openssl="openssl-1.1.1t"
+OPENSSL_ROOT="/tmp/$openssl"
 BOOST_ROOT="/tmp/boost_1_82_0"
 [[ -d $ZLIB_ROOT ]] || curl -sL https://zlib.net/zlib-1.2.13.tar.gz | tar zxf - -C /tmp
-[[ -d $OPENSSL_ROOT ]] || curl -sL https://www.openssl.org/source/openssl-3.1.0.tar.gz | tar zxf - -C /tmp
+[[ -d $OPENSSL_ROOT ]] || curl -sL "https://www.openssl.org/source/${openssl}.tar.gz" | tar zxf - -C /tmp
 [[ -d $BOOST_ROOT ]] || curl -sL https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.gz | tar zxf -
 
 target=( 
-    aarch64:darwin64-arm64
-    x86_64:darwin64-x86_64
+    # aarch64:darwin64-arm64
+    # x86_64:darwin64-x86_64
+    # above for openssl-3.1.x, below for openssl-1.1.1x
+    aarch64:darwin64-arm64-cc
+    x86_64:darwin64-x86_64-cc
 )
 
 for i in "${target[@]}";do
@@ -28,11 +33,11 @@ for i in "${target[@]}";do
     # build openssl
     rm -rf /tmp/_build/*
     $OPENSSL_ROOT/Configure \
-    ${t[1]} no-tests no-shared no-module \
+    ${t[1]} no-tests no-shared \
     no-autoload-config no-deprecated \
     --cross-compile-prefix=${t[0]}-apple-darwin21.4- --prefix=$PREFIX
-    make -j8
-    make install_sw
+    make -j8 build_libs
+    make install_dev
     rm -rf "$PREFIX/{bin,share}"
     # build boost
     echo "using gcc :  : ${t[0]}-apple-darwin21.4-clang++ ;" > /tmp/lb.jam
