fabe3dade4e812c65646562b8afa68a0d8f300bb
jiangwei <wwpptogether@gmail.com>
2023-05-04 12:46:44 +0800

    install gh

--- ins_pack.sh
+++ ins_pack.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+| dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" \
+| tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
 curl -fsSL https://deb.nodesource.com/setup_20.x | bash - 
-apt-get install -y nodejs p7zip-full rsync
+apt-get install -y nodejs p7zip-full rsync gh
 
 function buildAndCpLibs {
     ./linux_build.sh lib
